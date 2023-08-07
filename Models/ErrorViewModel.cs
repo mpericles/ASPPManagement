dbfff40075b827460732680aa9c0c89dd4cc2ba4
mpericles <mpericles1@yahoo.com>
2023-08-07 13:59:21 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPPManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
